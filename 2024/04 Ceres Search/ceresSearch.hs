34056b74f3ad09cdba5431890403cd10cad461e7
Rodrigo Llull Torres <rllullt@gmail.com>
2024-12-13 00:28:12 -0300

    Day 4 Part 1

--- 2024/04 Ceres Search/ceresSearch.hs
+++ 2024/04 Ceres Search/ceresSearch.hs
@@ -1,3 +1,5 @@
+import Text.Regex.TDFA
+
 main = do
   contents <- getContents
   -- Generar una lista con:
@@ -19,7 +21,6 @@ main = do
   let horLines = lines contents
       reversedHorLines = map reverse horLines
       n = length $ horLines !! 0
-      -- horLinesWDots = (map (\i -> horLines !! (i-1) ++ replicate i '.') [1..n]) ++ [replicate i '.' | i <- [n, n-1 .. 1]]
       horLinesWDots = (map (\i -> hLs !! i ++ replicate (2*n) '.') [0..2*n-1])
         where hLs = horLines ++ [replicate (2*n) '.' | i <- [0..n-1]]
       reversedHorLinesWDots = (map (\i -> hLs !! i ++ replicate (2*n) '.') [0..2*n-1])
@@ -27,17 +28,18 @@ main = do
       verLines = extractVerLines 0 horLines
       diagLinesIzqDer = extractDiagLines n 3 0 (+1) horLinesWDots
       diagLinesDerIzq = extractDiagLines n 3 0 (+1) reversedHorLinesWDots
-  print $ horLinesWDots
-  print $ verLines
-  print $ diagLinesIzqDer
-  print $ diagLinesDerIzq
+  -- print $ horLinesWDots
+  -- print $ verLines
+  -- print $ diagLinesIzqDer
+  -- print $ diagLinesDerIzq
+  let xmasCount = sum $ map countXmas [horLinesWDots, verLines, diagLinesIzqDer, diagLinesDerIzq]
+  print $ xmasCount
 
 extractVerLines :: Int -> [String] -> [String]
 extractVerLines k [] = []
 extractVerLines k hLs
   | (length hLs) == k = []
   | otherwise         = (map (\l -> l !! k) hLs) : extractVerLines (k+1) hLs
-
 
 extractDiagLines :: Int -> Int -> Int -> (Int -> Int) -> [[Char]] -> [[Char]]
 extractDiagLines n i j f_j [] = []
@@ -50,3 +52,9 @@ extractDiagLine i j f_j [] = []
 extractDiagLine i j f_j ls
   | i == -1 = []
   | otherwise = ls !! i !! j : extractDiagLine (i-1) (f_j j) f_j ls
+
+countXmas :: [String] -> Int
+countXmas = sum . map (\l -> textMatches l + textMatches (reverse l))
+    where pat = "XMAS" -- RegExp
+          textMatches :: String -> Int
+          textMatches l = length $ getAllTextMatches (l =~ pat :: AllTextMatches [] String)
