ded314f0d3aad9ddfc5c352b60e6c87388f10d8d
Rodrigo Llull Torres <rllullt@gmail.com>
2026-02-02 00:14:29 -0300

    Day 1 Part 2

--- 2025/01 Secret Entrance/secretEntrance.hs
+++ 2025/01 Secret Entrance/secretEntrance.hs
@@ -7,25 +7,38 @@ data Direction = L | R
 data Instruction = Instruction Direction Int
   deriving (Show, Eq)
 
-{-- First part
---}
 main = do
   contents <- getContents
   let dialPos = 50
       instructions = mapMaybe parseRotation $ lines contents  -- mapMaybe is a map, but omits (filters out) Nothing values in the list
-      rotations = zipWith rotate (dialPos : rotations) instructions
-      result = foldl (\x y -> if y == 0 then 1 + x else x) 0 rotations
+      -- rotations = zipWith rotate (dialPos : rotations) instructions
+      -- result = foldl (\x y -> if y == 0 then 1 + x else x) 0 rotations
+      rotations = zipWith rotate2 ((dialPos, 0) : rotations) instructions
+      result = foldl rotate2 (dialPos, 0) instructions
       
-      -- prediction = predictPath theMap  -- returns the map with the 'X'
-      -- distinctPositions = sum $ map (length . filter (=='X')) prediction
---  print $ instructions
---  print $ rotations
+  -- print $ instructions
+  -- print $ rotations
   print $ result
---}
 
 rotate :: Int -> Instruction -> Int
 rotate dialPos (Instruction L count) = (dialPos - count) `mod` 100
 rotate dialPos (Instruction R count) = (dialPos + count) `mod` 100
+
+rotate2 :: (Int, Int) -> Instruction -> (Int, Int)
+rotate2 (dialPos, count) (Instruction L amount) = (position, passesOver0 + count)
+  where
+    position = (dialPos - amount) `mod` 100
+    passesOver0 = if dialPos == 0
+                  then amount `div` 100
+                  else if amount == dialPos
+                       then 1
+                       else if amount > dialPos
+                            then 1 + (abs (dialPos - amount) `div` 100)
+                            else 0
+rotate2 (dialPos, count) (Instruction R amount) = (position, passesOver0 + count)
+  where
+    position = (dialPos + amount) `mod` 100
+    passesOver0 = (dialPos + amount) `div` 100
 
 parseRotation :: String -> Maybe Instruction
 parseRotation []     = Nothing
